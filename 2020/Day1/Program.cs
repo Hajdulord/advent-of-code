b64a1f3e40a716214cd6420bbc538594f5418fe1
Hajdu Marcell Ferenc <hajdulord@gmail.com>
2020-12-01 13:47:58 +0100

    Finished Day1

---
+++ 2020/Day1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static int Solver(List<int> inputs)
+        {
+            for (int i = 0 ; i < inputs.Count - 1; i++)
+            {
+                for (int j = inputs.Count - 1; j >= 0; j--)
+                {
+                    if(inputs[i] + inputs[j] == 2020)
+                    {
+                        return inputs[i] * inputs[j];
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        static int Solver2(List<int> inputs)
+        {
+            for (int i = 0; i < inputs.Count - 1; i++)
+            {
+                for (int j = inputs.Count - 1; j >= 0; j--)
+                {
+                    for (int k = inputs.Count - 1; k >= 0; k--)
+                    {
+                        if (inputs[i] + inputs[j] + inputs[k] == 2020)
+                        {
+                            return inputs[i] * inputs[j] * inputs[k];
+                        }
+                    }
+                    
+                }
+            }
+
+            return -1;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solver(File.ReadAllLines("input.txt").ToList().ConvertAll(int.Parse)));
+            Console.WriteLine(Solver2(File.ReadAllLines("input.txt").ToList().ConvertAll(int.Parse)));
+        }
+    }
+}
